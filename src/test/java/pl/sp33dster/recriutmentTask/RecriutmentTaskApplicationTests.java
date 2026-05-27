b55e6070c7e99532a41ddef86fd15345a149030d
Sp33dyster <bartlomiej_gajewski1@epam.com>
2026-05-27 18:09:30 +0200

    initial commit

---
+++ src/test/java/pl/sp33dster/recriutmentTask/RecriutmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sp33dster.recriutmentTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecriutmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
